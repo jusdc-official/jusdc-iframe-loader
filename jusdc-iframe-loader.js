c85ae084fe072598ea29ce631e42e2a83facf51e
JUSDC-Official <info@jusdc.io>
2025-08-12 16:43:32 +0400

    Create jusdc-iframe-loader.js

---
+++ jusdc-iframe-loader.js
@@ -0,0 +1,12 @@
+<head>
+  <!-- CSS (if not auto-injected) -->
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <!-- Your iframes here -->
+  <script src="js/jusdc-iframe-loader.js"></script>
+  <script>
+    // Initialize
+    loadJUSDCIframe('jusdcDashboard', 'https://app.jusdc.io/dashboard');
+  </script>
+</body>
